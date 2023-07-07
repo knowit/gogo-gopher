a05264c3699cbd0f3d35710801b2b9471d54b6ed
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2023-07-07 13:10:16 +0200

    Adds sql schema and query

---
+++ cruddy/query.sql
@@ -0,0 +1,26 @@
+-- name: GetAuthor :one
+SELECT * FROM authors
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAuthors :many
+SELECT * FROM authors
+ORDER BY name;
+
+-- name: CreateAuthor :one
+INSERT INTO authors (
+  name, bio
+) VALUES (
+  $1, $2
+)
+RETURNING *;
+
+-- name: UpdateAuthor :one
+UPDATE authors
+  set name = $2,
+  bio = $3
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAuthor :exec
+DELETE FROM authors
+WHERE id = $1;
